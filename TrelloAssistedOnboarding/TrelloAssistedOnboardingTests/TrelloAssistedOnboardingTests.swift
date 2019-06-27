429d964ffd9bc8321943f2d3244685b272ddc3ea
Lou Franco <lfranco@atlassian.com>
2019-06-27 12:50:31 -0400

    Initial Commit

---
+++ TrelloAssistedOnboarding/TrelloAssistedOnboardingTests/TrelloAssistedOnboardingTests.swift
@@ -0,0 +1,34 @@
+//
+//  TrelloAssistedOnboardingTests.swift
+//  TrelloAssistedOnboardingTests
+//
+//  Created by Lou Franco on 6/27/19.
+//  Copyright © 2019 Trello. All rights reserved.
+//
+
+import XCTest
+@testable import TrelloAssistedOnboarding
+
+class TrelloAssistedOnboardingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
